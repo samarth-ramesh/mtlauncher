587350d46759059efb5d758990af1ae545a1fd1a
Samarth Ramesh <samarthr1@outlook.com>
2021-02-06 02:18:43 +0530

    added db support for history, improved login screen (thanks rubenwardy, lemente.

--- assets/sql/init.sql
+++ assets/sql/init.sql
@@ -1,2 +1,3 @@
 CREATE TABLE users ( username VARCHAR(20) UNIQUE, passwd VARCHAR(256));
 CREATE TABLE passwds (addr VARCHAR, port INT, uname VARCHAR, passwd BLOB, iv BLOB);
+CREATE TABLE history(addr VARCHAR, port INT, uname VARCHAR, timeAt VARCHAR);
