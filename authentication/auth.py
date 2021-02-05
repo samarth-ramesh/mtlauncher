4857910eebbeac9658c67dd05f69f62ef98f1e7f
Samarth Ramesh <samarthr1@outlook.com>
2021-02-05 19:30:10 +0530

    added copyright notice

--- authentication/auth.py
+++ authentication/auth.py
@@ -1,3 +1,8 @@
+# This is free software licensed under the MIT license.
+# Copyright (c) 2021 Samarth Ramesh <samarthr1@outlook.com>
+# You should have recived a copy of the MIT license with this file. In case you ahve not, visit https://github.com/samarth-ramsh/mtlaucher
+
+
 import hashlib
 import sqlite3
 import os
--- sessionVars/__init__.py
+++ sessionVars/__init__.py
@@ -1,3 +1,7 @@
+# This is free software licensed under the MIT license.
+# Copyright (c) 2021 Samarth Ramesh <samarthr1@outlook.com>
+# You should have recived a copy of the MIT license with this file. In case you ahve not, visit https://github.com/samarth-ramsh/mtlaucher
+
 password = ""
 username = ""
 
